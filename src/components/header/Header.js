3b7ab64a7e5c18c778a7841bfe9fa9f5b22aa23e
Kh82Olga <kh82olga@protonmail.com>
2022-12-21 15:20:52 +0100

    button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import './headerStyle.css';
+import React from "react";
+import "./headerStyle.css";
 const Header = () => {
   return (
     <header className="header">
-        <div className="header__wrapper">
-          <h1 className="header__title">
-            <strong>
-              Hi, my name is <em>Olga</em>
-            </strong>
-            <br />a Fullstack Developer
-          </h1>
-          <div className="header__text">
-            <p>with passion for learning and creating.</p>
-          </div>
-          <a href="#!" className="btn">
-            Download CV
-          </a>
+      <div className="header__wrapper">
+        <h1 className="header__title">
+          <strong>
+            Hi, my name is <em>Olga</em>
+          </strong>
+          <br />a Fullstack Developer
+        </h1>
+        <div className="header__text">
+          <p>with passion for learning and creating.</p>
         </div>
-      </header>
-  )
-}
+        <a
+          href="/download_cv/Kharchuk_Olga_CV_nl.pdf"
+          target={"_blank"}
+          /* download */
+          className="btn"
+        >
+          Download CV
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
