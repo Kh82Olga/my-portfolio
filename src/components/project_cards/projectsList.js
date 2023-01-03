6732cc7b2aa951c61aac5a5a83e9da4988c8c284
Kh82Olga <kh82olga@protonmail.com>
2023-01-03 18:35:06 +0100

    0301

--- src/components/project_cards/projectsList.js
+++ src/components/project_cards/projectsList.js
@@ -1,18 +1,18 @@
-import project1 from "./../../img/projects/01.jpg";
+import project1 from "./../../img/projects/03.jpg";
 import project2 from "./../../img/projects/02.jpg";
-import project1big from "./../../img/projects/01-big.jpg";
+import project1big from "./../../img/projects/03-big.jpg";
 import project2big from "./../../img/projects/02-big.jpg";
 
 const allProjects = [
   {
-    title: "Project 1",
-    skills: "React, Node.js, MongoDB",
+    title: "Cinema Ticket",
+    skills: "C#, ASP.NET Core MVC, Entity Framework, SQL, Bootstrap",
     img: project1,
     imgBig: project1big,
-    gitHubLink: "https://github.com/Kh82Olga",
+    gitHubLink: "https://github.com/Kh82Olga/EindWerk_CinemaTicket",
   },
   {
-    title: "Project 2",
+    title: "Coming soon...",
     skills: "Angular, HTML, CSS, MySQL",
     img: project2,
     imgBig: project2big,
